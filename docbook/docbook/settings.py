cd58540e1d0813529cd5b3986783d5b6e5de7fe6
Raunak sharma <iamroni98177@gmail.com>
2026-03-09 20:37:10 +0530

    fixed render root directory structure

--- docbook/docbook/settings.py
+++ docbook/docbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yequ0dvqeum56)20_o=h%px79d)4t-&(na9llsb4!7qop=39ov
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,5 +147,8 @@ SIMPLE_JWT = {
 }
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
